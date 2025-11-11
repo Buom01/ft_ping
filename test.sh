ffdee6b8b3924f2664be1c2296bad2408084d2d0
Bastien ADAM <badam@student.42.fr>
2025-11-11 03:18:10 +0100

    Add tester

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <duration_in_seconds> [ping_options]"
+  exit 1
+fi
+
+./ft_ping "${@:2}" > ft_ping.txt &
+sleep $1
+pkill --signal SIGINT ft_ping
+
+ping "${@:2}" > ping.txt &
+sleep $1
+pkill --signal SIGINT ping
+
+diff -u ping.txt ft_ping.txt
